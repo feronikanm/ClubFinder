0d363491874c0ed9fda009264dc3f51f193495ac
Feronika <firaferonika27@gmail.com>
2020-09-07 21:23:31 +0700

    club finder

---
+++ src/script/data/data-source.js
@@ -0,0 +1,18 @@
+import clubs from './clubs.js';
+
+class DataSource{
+
+    static searchClub(keyword) {
+        return new Promise((resolve, reject) => {
+            const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
+    
+            if (filteredClubs.length) {
+                resolve(filteredClubs);
+            } else {
+                reject(`${keyword} is not found`);
+            }
+        }); 
+    }
+}
+
+export default DataSource;
---
+++ src/script/view/main.js
@@ -0,0 +1,30 @@
+import '../component/club-list.js';
+import '../component/search-bar.js';
+import DataSource from '../data/data-source.js';
+
+const main = () => {
+    const searchElement = document.querySelector("search-bar");
+    const clubListElement = document.querySelector("club-list");
+
+    const onButtonSearchClicked = async () => {
+        try{
+            const result = await DataSource.searchClub(searchElement.value);
+            renderResult(result);
+        }catch (message){
+            fallbackResult(message)
+        }
+    };
+
+    const renderResult = results => {
+        clubListElement.clubs = results;
+        
+    };
+
+    const fallbackResult = message => {
+        clubListElement.renderError(message);
+    };
+
+    searchElement.clickEvent = onButtonSearchClicked;
+};
+
+export default main;

ee09845383049745dfcd6e9a214d3fb628f13d7f
Feronika <firaferonika27@gmail.com>
2020-09-07 21:30:59 +0700

    fetch api

--- src/script/component/club-item.js
+++ src/script/component/club-item.js
@@ -50,10 +50,10 @@ class ClubItem extends HTMLElement{
                    -webkit-line-clamp: 10; /* number of lines to show */
                }
            </style>
-           <img class="fan-art-club" src="${this._club.fanArt}" alt="Fan Art">
+           <img class="fan-art-club" src="${this._club.strTeamBadge}" alt="Fan Art">
            <div class="club-info">
-               <h2>${this._club.name}</h2>
-               <p>${this._club.description}</p>
+               <h2>${this._club.strTeam}</h2>
+               <p>${this._club.strDescriptionEN}</p>
            </div>`;
     }
 }
--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -3,15 +3,17 @@ import clubs from './clubs.js';
 class DataSource{
 
     static searchClub(keyword) {
-        return new Promise((resolve, reject) => {
-            const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
-    
-            if (filteredClubs.length) {
-                resolve(filteredClubs);
+        return fetch(`https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${keyword}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(responseJson => {
+            if(responseJson.teams) {
+                return Promise.resolve(responseJson.teams);
             } else {
-                reject(`${keyword} is not found`);
+                return Promise.reject(`${keyword} is not found`);
             }
-        }); 
+        })
     }
 }
 
